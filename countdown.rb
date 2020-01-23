c186d0fc898c601541c2d56eaf958aa9e21e4571
Daniel J Feleppa <dfeleppa@gmail.com>
2020-01-23 23:43:24 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,12 +9,3 @@ end
 countdown(10)
 
 
-def countdown_with_sleep(number)
-  while number > 0
-    "#{number} SECOND(S)!"
-    number -= 1
-    sleep(1)
-  end
-  return "HAPPY NEW YEAR!"
-end
-countdown_with_sleep(10)
