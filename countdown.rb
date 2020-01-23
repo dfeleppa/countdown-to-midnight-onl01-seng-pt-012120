8e53188f8aadef0e03c3234acfeeef3056345dc6
Daniel J Feleppa <dfeleppa@gmail.com>
2020-01-23 23:38:58 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,27 +1,22 @@
-#write your code here
-
-
 
 def countdown(number)
-  number = 0 
-  While number > 0
-    "#{number} SECOND(S)!"
+  while number > 0
+   sleep 1
+    puts "#{number} SECOND(S)!"
     number -= 1
   end
   return "HAPPY NEW YEAR!"
-  end
 end
 
 countdown(10)
 
 def countdown_with_sleep(number)
-  number = 0 
-  While number > 0
-    "#{number} SECOND(S)!"
+  while number > 0
+   sleep 1
+    puts "#{number} SECOND(S)!"
     number -= 1
-    sleep(1)
   end
   return "HAPPY NEW YEAR!"
 end
 
-countdown_with_sleep(10)
+countdown(10)
